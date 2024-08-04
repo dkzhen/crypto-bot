85343e76e07f8c08ff051e788a18f7ed2fad10c3
dkzhen <dani314123@gmail.com>
2024-08-04 13:20:58 +0700

    feat : add nomis bot auth feedback

--- src/projects/nomis/CheckValidToken.js
+++ src/projects/nomis/CheckValidToken.js
@@ -15,6 +15,15 @@ exports.validateToken = async () => {
       await getProfile(token.token, user_id);
       validToken.push(token);
     } catch (error) {
+      const url = `${process.env.API_URL}/bot/sendMessage`;
+      await axios.post(url, {
+        chatId: token.telegramId,
+        message: `Token expired or invalid: 
+ \nBot : @NomisBot 
+ \nTelegramId : ${token.telegramId} \nToken : ${token.token}`,
+        tokenId: token.id,
+      });
+
       console.log(`[ Error ] : token not valid , response code : ${error}`);
     }
   }
