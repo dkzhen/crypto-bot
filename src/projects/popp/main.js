37f7e3224643a9bd39c60fdeb0a36fac46974d22
dkzhen <dani314123@gmail.com>
2024-08-06 11:45:02 +0700

    feat: task social summer for popp

--- src/projects/popp/main.js
+++ src/projects/popp/main.js
@@ -4,16 +4,19 @@ const { configDotenv } = require("dotenv");
 const { rewardDaily } = require("./DailyReward");
 const { farming } = require("./farming");
 const { planetGame } = require("./ExplorePlanet");
+const { SocialSummerTask } = require("./VisitSummer");
 
 configDotenv();
 
 rewardDaily();
 farming();
 planetGame();
+SocialSummerTask();
 
 cron.schedule("0 * * * *", rewardDaily);
 cron.schedule("0 * * * *", farming);
 cron.schedule("0 * * * *", planetGame);
+cron.schedule("0 * * * *", SocialSummerTask);
 const app = express();
 
 const port = process.env.PORT_POPP || 201;
